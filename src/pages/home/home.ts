452c3e83a742a70a563d3ee360c23a534ab14aa5
Fernando Augusto Santos <fstrony@gmail.com>
2020-02-25 20:07:15 -0300

    FIAP PART 2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,31 +1,41 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { DetailPage } from '../detail/detail';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { GamesProvider } from '../../providers/games/games';
 
 @Component({
- selector: 'page-home',
- templateUrl: 'home.html'
+  selector: 'page-home',
+  templateUrl: 'home.html'
 })
-
 export class HomePage {
- games: object[] = []
- constructor(
-   public gamesProvider: GamesProvider,
-   public navCtrl: NavController,
-   public navParams: NavParams) {
-     this.games = this.gamesProvider.games
- }
- 
- convertObjectToString(obj) {
-   return JSON.stringify(obj)
- }
-
- convertPlatformToString(platform) {
-   return platform.join(' - ')
- }
- 
- openDetail(game, index) {
-   this.navCtrl.push(DetailPage, { game, index })
- }
+  games: object[] = []
+  constructor(
+    public barcodeScanner: BarcodeScanner,
+    public gamesProvider: GamesProvider,
+    public alertCtrl: AlertController,
+    public navCtrl: NavController,
+    public navParams: NavParams) {
+      this.games = this.gamesProvider.games
+  }
+  convertObjectToString(obj) {
+    return JSON.stringify(obj)
+  }
+  convertPlatformToString(platform) {
+    return platform.join(' - ')
+  }
+  openDetail(game, index) {
+    this.navCtrl.push(DetailPage, { game, index })
+  }
+  openCamera() {
+    this.barcodeScanner.scan().then(barcodeData => {
+      const alert = this.alertCtrl.create({
+        title: 'Dados do QrCode',
+        message: barcodeData.text
+      })
+      alert.present()
+    }, (err) => {
+      console.log('Error: ', err);
+    });
+  }
 }
